6dc58e041d2601e850bceb0b01248201f8294495
EdgarCallerosM <edgarcallerosm@outlook.com>
2026-09-22 04:11:16 -0600

    Fix SQL view to replace null values with 'Desconocido'

--- SQL_Views/ClientesLimpios.sql
+++ SQL_Views/ClientesLimpios.sql
@@ -1,10 +1,10 @@
 /*Las columnas Nombre, Apellido y Estado tienen valores Nulos. Aqui simplemente estamos reemplazando esos nulos con el texto generico "Desconocido".*/
 CREATE VIEW ClientesLimpios AS
 SELECT DISTINCT
-ClienteID,
-COALESCE(Nombre,'Desconocido') Nombre,
-COALESCE(Apellido,'Desconocido') Apellido,
-COALESCE(Estado,'Desconocido') Estado,
-FechaNacimiento,
-Segmento
+    ClienteID,
+    COALESCE(Nombre,'Desconocido') Nombre,
+    COALESCE(Apellido,'Desconocido') Apellido,
+    COALESCE(Estado,'Desconocido') Estado,
+    FechaNacimiento,
+    Segmento
 FROM Clientes 
