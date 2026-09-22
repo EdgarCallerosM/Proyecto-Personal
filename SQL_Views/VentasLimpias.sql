54f782bfc7f7e4625b5cfdc25060362cf0ac75d9
EdgarCallerosM <edgarcallerosm@outlook.com>
2026-09-22 04:10:59 -0600

    Modify VentasLimpias view to join Productos table
    Added LEFT JOIN to include product details in the view.

--- SQL_Views/VentasLimpias.sql
+++ SQL_Views/VentasLimpias.sql
@@ -1,20 +1,21 @@
 --En este query, nos deshacemos de las ventas repetidas y reemplazamos los precios en la tabla Ventas con los precios oficiales de la tabla Productos.
 CREATE VIEW VentasLimpias AS
 SELECT DISTINCT
-v.VentaID,
-v.ClienteID,
-v.ProductoID,
-p.Producto,
-p.Categoria,
-p.Proveedor,
-v.FechaVenta,
-v.Cantidad,
-v.MetodoPago,
-v.Canal,
-v.EstadoVenta,
-p.Precio,
-v.DescuentoPct,
-p.precio*v.Cantidad*(1-v.DescuentoPct/100.0) Ventas
+    v.VentaID,
+    v.ClienteID,
+    v.ProductoID,
+    p.Producto,
+    p.Categoria,
+    p.Proveedor,
+    v.FechaVenta,
+    v.Cantidad,
+    v.MetodoPago,
+    v.Canal,
+    v.EstadoVenta,
+    p.Precio,
+    v.DescuentoPct,
+    p.precio*v.Cantidad*(1-v.DescuentoPct/100.0) Ventas
 FROM Ventas v
+  
 LEFT JOIN Productos p
 ON p.ProductoID=v.ProductoID
