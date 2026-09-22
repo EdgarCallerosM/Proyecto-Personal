5bb4eebbb87a800b461d69aa388858a9b4d61f65
EdgarCallerosM <edgarcallerosm@outlook.com>
2026-09-22 02:00:36 -0600

    Comentarios añadidos

--- SQL_Queries/14_NombresApellidosConEspacios.sql
+++ SQL_Queries/14_NombresApellidosConEspacios.sql
@@ -1,3 +1,5 @@
+/* Si utilizamos el comparador logico = para comparar TRIM(texto) y texto, SQL server ignora si hay espacios en blanco al final de texto. 
+Es por eso que usamos "NOT LIKE" para hacer la comparacion.*/
 SELECT
 ClienteID,
 Nombre,
